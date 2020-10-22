e0cc77050d09eca74376f102c855a1fdca3aa113
JinSeok <seok8440@naver.com>
2020-10-22 23:19:56 +0900

    머스테치 다운 및 파일 추가

--- src/main/java/com/spr/bt/springboot/web/PostsApiController.java
+++ src/main/java/com/spr/bt/springboot/web/PostsApiController.java
@@ -19,7 +19,7 @@ public class PostsApiController {
     }
 
     @PutMapping("/api/v1/posts/{id}")
-    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
         return postsService.update(id, requestDto);
     }
 
